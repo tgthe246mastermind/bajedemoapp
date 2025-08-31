3fbfc52e96b7d3dfa4b11fcbcdf82920ff823418
tgthe246mastermind <137459085+tgthe246mastermind@users.noreply.github.com>
2025-08-31 16:22:35 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import { createClient } from '@supabase/supabase-js';
+
 import { useEffect } from "react";
 
 import Baje from './Baje.jsx';
 
 
-const supabase = createClient(
-  "https://lgurtucciqvwgjaphdqp.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxndXJ0dWNjaXF2d2dqYXBoZHFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk2MzgzNTAsImV4cCI6MjA0NTIxNDM1MH0.I1ajlHp5b4pGL-NQzzvcVdznoiyIvps49Ws5GZHSXzk"
-);
+
 
 
 
 function App() {
-  const navigate = useNavigate();
-
-  // Integrate global session refresh
-  useAuthRefresh();
-
-/
+ 
 
   return (
    
