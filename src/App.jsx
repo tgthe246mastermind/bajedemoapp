a7a05349ec97ec2372c419e74a7b05b01667310b
tgthe246mastermind <137459085+tgthe246mastermind@users.noreply.github.com>
2025-09-02 11:30:28 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
-
-
-
+import Signup1 from './Signup1.jsx';
 import Baje from './Baje.jsx';
-
+import Login from './Login.jsx';
+import Home from './Home.jsx';
+import Onboarding from './Onboarding.jsx';
+import ForgotPassword from './ForgotPassword.jsx';
+import ResetPassword from './ResetPassword.jsx';
+import Loadscreen from './Loadscreen.jsx';
 
 
 
@@ -14,11 +17,17 @@ function App() {
 
   return (
    
-      <Routes>
-        <Route path="/" element={<Baje />} />
-       
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup1 />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/baje" element={<Baje />} />
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/loadscreen" element={<Loadscreen />} />
+        <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
-   
   );
 }
 
